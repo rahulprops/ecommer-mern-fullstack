d835f4fb817bc08e7e314021be597d099c2bc9da
rahulprops <rahulhack912@gmail.com>
2025-03-18 14:45:46 +0530

    ordermodel create

---
+++ models/order.model.js
@@ -0,0 +1,63 @@
+import mongoose, { model, Schema } from "mongoose";
+import { type } from "os";
+
+const orderSchema=new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user"
+    },
+    orderItems:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"orderItems",
+    }],
+    orderDate:{
+        type:Date,
+        required:true,
+        default:Date.now()
+    },
+    deliveryDate:{
+        type:Date,
+    },
+    shippingAddress:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"addresses",
+    },
+    paymentDetails:{
+         paymentMethod:{
+            type:String,
+         },
+         transactionId:{
+            type:String,
+         },
+         paymentId:{
+            type:String,
+         },
+         paymentStatus:{
+            type:String,
+            default:"PENDING",
+         }
+    },
+  totalPrice:{
+    type:Number,
+    required:true,
+  },
+  totalDiscountedPrice:{
+    type:Number,
+    required:true
+  },
+  discounte:{
+    type:Number,
+    required:true,
+  },
+  orderStatus:{
+    type:String,
+    required:true,
+    default:"PENDING"
+  },
+  totalItem:{
+    type:Number,
+    required:true,
+  },
+},{timestamps:true})
+const orderModel=model("order",orderSchema)
+export default orderModel;
