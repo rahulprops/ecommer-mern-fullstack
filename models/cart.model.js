62d6396f05f850fef9afe5d3486cfa1d23eeacda
rahulprops <rahulhack912@gmail.com>
2025-03-18 14:20:21 +0530

    cart model create

---
+++ models/cart.model.js
@@ -0,0 +1,36 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const cartSchema=new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"user",
+        required:true,
+    },
+    cartItems:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"cartItems",
+        required:true,
+    }],
+    totalPrice:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    totalItem:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    totalDiscoutedPrice:{
+        type:Number,
+        required:true,
+        default:0,
+    },
+    discounte:{
+        type:Number,
+        required:true,
+        default:0,
+    }
+},{timeseries:true})
+const cartModel=model("cart",cartSchema)
+export default cartModel;
