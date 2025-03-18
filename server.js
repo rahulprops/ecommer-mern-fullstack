c4e6e539c9fb25de406eb43d9bae2505708c4ba8
rahulprops <rahulhack912@gmail.com>
2025-03-18 13:52:23 +0530

    connect database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import 'dotenv/config.js'
+import dbConnect from './config/db.js'
 
 const app=express()
 const port = process.env.PORT || 3000
@@ -7,4 +8,5 @@ const port = process.env.PORT || 3000
 //! server start
 app.listen(port,()=>{
     console.log(`server is running on prot http://localhost/${port}`)
+    dbConnect()
 })
